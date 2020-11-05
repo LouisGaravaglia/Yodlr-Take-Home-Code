aa9425ce7bf3d77dfd0d3fde1d351a40e834fd1c
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-05 13:12:56 -0800

    set foundation for registration form

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,10 +4,11 @@ import {NavLink} from "react-router-dom";
 const NavBar = () => {
   
         return (
-            <>
+    
+            <div className="NavBar">
                 <NavLink exact to="/signup">Register</NavLink>
                 <NavLink exact to="/admin">Admin</NavLink>
-            </>
+            </div>
         )
     }
 
--- src/Register.js
+++ src/Register.js
@@ -1,11 +1,54 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import "./App.css";
 import UserContext from "./UserContext";
 
 const Register = () => {
+  const INITIAL_STATE =  {
+    email: "",
+    firstName: "",
+    lastName: ""
+  }
+  const [formData, setFormData] = useState(INITIAL_STATE);
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = () => {
+    
+  }
 
   return (
-    <h1>Hiiiii</h1>
+    <div>
+    <h3>Register</h3>
+    <form onSubmit={handleSubmit}>
+    <label htmlFor="firstName">First Name:</label>
+    <input
+      type="text"
+      name="firstName"
+      id="firstName"
+      value={formData.firstName}
+      onChange={handleChange}
+    />
+    <label htmlFor="lastName">Last Name:</label>
+      <input
+        type="text"
+        name="lastName"
+        id="lastName"
+        value={formData.lastName}
+        onChange={handleChange}
+      />
+    <label htmlFor="email">Email:</label>
+      <input
+        type="text"
+        name="email"
+        id="email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+      <button>Submit</button>
+    </form>
+    </div>
   )
 }
 
